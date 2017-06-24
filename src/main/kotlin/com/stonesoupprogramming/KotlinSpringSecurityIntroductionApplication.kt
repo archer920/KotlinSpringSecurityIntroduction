b020b1601cde2191178c3c82ad43cc70b6a5af04
Patrick Luck <stonesoup@Patricks-MacBook-Pro.local>
2017-06-24 12:01:23 -0400

    Initial Commit

---
+++ src/main/kotlin/com/stonesoupprogramming/KotlinSpringSecurityIntroductionApplication.kt
@@ -0,0 +1,11 @@
+package com.stonesoupprogramming
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class KotlinSpringSecurityIntroductionApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(KotlinSpringSecurityIntroductionApplication::class.java, *args)
+}
